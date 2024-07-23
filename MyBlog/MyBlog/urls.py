270272bc4f37f3fb950d80237c8cd11ffa8393c0
kartik01112004 <kartikgoel018@gmail.com>
2024-07-23 10:23:37 +0530

    adding models

--- MyBlog/MyBlog/urls.py
+++ MyBlog/MyBlog/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from BlogApp.views import *
-from helloWorld.views import *
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', viewIndex , name="home"),
-    path('hello/', viewsHello , name="hello")
+    path('blog/',include('BlogApp.urls'))
 ]
